9c354f665559701a3e5da6dec2ef9f952a9b2667
Engin Karataş <enginkaratas99@gmail.com>
2023-07-18 19:43:19 +0300

    create GameManager.cs

---
+++ crossword-puzzle-game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    // You can define variables and references to other scripts/components here
+
+    // Use this for initialization
+    void Start()
+    {
+        // Your initialization code goes here
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Your game update code goes here
+    }
+
+    // You can add other methods as needed to manage game state, transitions, etc.
+}
